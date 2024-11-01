62d0e99a937f7929b70a2b271d1d5e7ef7d3eac6
devcontainerDuy <125916124+devcontainerDuy@users.noreply.github.com>
2024-11-01 14:44:44 +0700

    update sanctum

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware(['auth:sanctum', 'web'])->get('/user', function (Request $requ
 Route::middleware('api')->group(function () {
     Route::controller(App\Http\Controllers\Auth\AuthenticateController::class)->group(function () {
         Route::post('register', 'register');
-        Route::post('login', 'login')->middleware('auth:customer');
+        Route::post('login', 'login');
         Route::post('logout', 'logout')->middleware('auth:sanctum');
     });
 });
