6fb4df86678d1a41d390344e8018abf038eecd43
devcontainerDuy <khanhduytran1803@gmail.com>
2024-12-04 08:07:20 +0700

    Refactor handleAuthCallback method to remove JSON response type hint

--- app/Http/Controllers/Auth/Clients/AuthenticateController.php
+++ app/Http/Controllers/Auth/Clients/AuthenticateController.php
@@ -9,6 +9,7 @@ use App\Mail\createUser;
 use App\Models\Customers;
 use App\Traits\GeneratesUniqueId;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
@@ -85,7 +86,7 @@ class AuthenticateController extends Controller
         ]);
     }
 
-    public function handleAuthCallback(): JsonResponse
+    public function handleAuthCallback()
     {
         try {
             $user = Socialite::driver('google')->stateless()->user();
