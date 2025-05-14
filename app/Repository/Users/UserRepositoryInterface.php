021b3a7d9291f786e0f737242afe9cd354a13451
devcontainerDuy <khanhduytran1803@gmail.com>
2025-05-14 13:10:31 +0700

    feat: define UserRepositoryInterface and EloquentRepositoryInterface; implement EloquentRepository class

--- app/Repository/Users/UserRepositoryInterface.php
+++ app/Repository/Users/UserRepositoryInterface.php
@@ -4,5 +4,9 @@ namespace App\Repository\Users;
 
 interface UserRepositoryInterface
 {
-    //
+    public function getAll(): array;
+    public function getById(int $id): ?array;
+    public function create(array $data): array;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
 }
